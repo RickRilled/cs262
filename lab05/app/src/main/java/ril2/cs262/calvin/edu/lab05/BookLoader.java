ba0fcbe6846bd9bc15c2a52e56c20d3c3a97eb10
Royce Lloyd <ril2@gold09.cs.calvin.edu>
2018-10-05 16:06:23 -0400

    added lab05

---
+++ lab05/app/src/main/java/ril2/cs262/calvin/edu/lab05/BookLoader.java
@@ -0,0 +1,32 @@
+package ril2.cs262.calvin.edu.lab05;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    BookLoader(Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    public BookLoader(@NonNull Context context) {
+        super(context);
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        super.forceLoad();
+    }
+}
